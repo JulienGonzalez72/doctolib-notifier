126745868f17adb864f9949695227e5a1536eb22
JulienGonzalez72 <julien.gonzalez72@gmail.com>
2023-02-26 16:20:11 +0100

    test #11

--- src/doctolibAPI.ts
+++ src/doctolibAPI.ts
@@ -22,6 +22,7 @@ export default class DoctolibAPI {
         'referrer-policy': 'origin-when-cross-origin',
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+        'Access-Control-Allow-Origin': '*',
       },
     })
   }
